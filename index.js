70c55ea6323fdfcce8c36a76bd97e2df20475f8f
dulce509 <aplicativodulce421@gmail.com>
2024-11-04 20:56:10 +0000

    # DesafioandDio_Felipao

---
+++ index.js
@@ -0,0 +1,25 @@
+
+let nickHeroi = Nebuloso
+let experienciaXp = " "
+let resultadoTotal = (nickHeroi + experiênciaXp)
+
+If(experienciaXp < 1000); {
+    console.log(Ferro)
+}else if (experienciaXp >= 1001 && < 2000); {
+    console.log(Bronze)
+}else if (experienciaXp >= 2001 && < 5000); {
+    console.log(Prata)
+}else if (experienciaXp >= 5001 && < 7000); {
+    console.log(Ouro)
+}else if (experienciaXp >= 7001 && < 8000); {
+    console.log(Platina)
+}else if (experienciaXp >= 8001 && < 9000); {
+    console.log(Ascendente)
+}else if (experienciaXP >= 9001 && < 10.000); {
+    console.log(Imortal);
+
+}else (experienciaXp >= 10.001); {
+    console.log(Radiante)
+}
+
+Console.log("O herói:", nickHeroi, "está no nível" + resultadoTotal)
